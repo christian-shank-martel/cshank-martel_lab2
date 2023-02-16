34d04757d219c0ff24269c074a3d4f8932857dd4
christian-shank-martel <61027159+christian-shank-martel@users.noreply.github.com>
2023-02-15 22:29:44 -0500

    Create Lab2.py

---
+++ Lab2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#Author: christian shank-martel 
+#AuthorID: cshank-martel
+#Date:Feb 15 2023
+#Program: Lab2.py
+import sys
+arguments = sys.argv
+Prog = sys.argv[0]
+
+var1=sys.argv[1]
+var2=sys.argv[2]
+
+print("program: ", Prog)
+print("Variable 1: ", var1)
+print("Variable 2: ", var2)
