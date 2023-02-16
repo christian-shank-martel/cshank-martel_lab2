314225f60204e9a63bdbddbbc7b2c3c48f0c5b13
christian-shank-martel <61027159+christian-shank-martel@users.noreply.github.com>
2023-02-16 00:38:20 -0500

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -7,9 +7,13 @@ import sys
 arguments = sys.argv
 Prog = sys.argv[0]
 
-var1=sys.argv[1]
-var2=sys.argv[2]
+def var_used():
+    var1=sys.argv[1]
+    var2=sys.argv[2]
 
-print("program: ", Prog)
-print("Variable 1: ", var1)
-print("Variable 2: ", var2)
+    print("program: ", Prog)
+    print("Variable 1: ", var1)
+    print("Variable 2: ", var2)
+
+if __name__ == "__main__":
+    var_used()
